580237cb141552c0b015562c1e87780a01c9703a
Takashi Murata <muratakashi@gmail.com>
2020-12-14 22:04:07 +0900

    CORS対策の設定を追加しました

--- yamagoya/main.py
+++ yamagoya/main.py
@@ -3,6 +3,7 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from starlette.requests import Request
 
@@ -11,13 +12,21 @@ from tripcost import models, schemas
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
+
 
 @app.get("/tripcost")
 def tripcost_findall(db: Session = Depends(get_db)):
@@ -32,5 +41,3 @@ def read_root():
 # @app.get("/items/{item_id}")
 # def read_item(item_id: int, q: Optional[str] = None):
 #     return {"item_id": item_id, "q": q}
-
-
